f560cb129218e03f00e20785973f44dd87c02d07
konstantin0s <constantintofan85@gmail.com>
2019-06-12 14:36:27 +0200

    Add movie logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import cartoon from './cartoon.png';
 
-function App() {
-  return (
-    <div className="App">
-<h1>Movies search</h1>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+          <div className="App">
+<table>
+  <tbody>
+    <tr>
+      <td>
+        <img style={{width: '50px'}} src={cartoon} />
+      </td>
+      <td>
+        MoviesDB Search
+      </td>
+    </tr>
+  </tbody>
+</table>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
