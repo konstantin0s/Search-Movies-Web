54ed1b189a15007288e803e3d6a99dea8d2833c8
konstantin0s <constantintofan85@gmail.com>
2019-06-13 13:10:43 +0200

    add title

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -14,10 +14,15 @@ class MovieRow extends React.Component {
     <tbody className="row">
       < tr className = "col col-md-12" >
         <td>
-          <img alt="poster" width="120" src={this.props.movie.poster_src}/>
+          <img alt="poster" src={this.props.movie.poster_src}/>
         </td>
         <td>
-          <h3>{this.props.movie.title}</h3>
+          <h3><i class="fa fa-quote-left"></i>
+          {
+            this.props.movie.title
+          }
+          <i class="fa fa-quote-right"></i>
+</h3>
           <p className="overview">{this.props.movie.overview}</p>
           <input className="btn btn-primary" type="button" onClick={this.viewMovie} target="_blank" value="View"/>
         </td>
