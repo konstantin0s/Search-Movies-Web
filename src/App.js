978dd9aa52e7e44241b494651fadf7f9a341d60c
konstantin0s <constantintofan85@gmail.com>
2019-06-13 12:31:17 +0200

    Fetch MoviesDB & display on page

--- src/App.js
+++ src/App.js
@@ -2,32 +2,71 @@ import React, { Component } from 'react';
 import './App.css';
 import cartoon from './cartoon.png';
 import MovieRow from './components/MovieRow';
+import $ from 'jquery';
+import axios from 'axios';
 
-const movies = [
-  { id: 0, img_src: 'http://watchingmovies.getenjoyment.net/images/cartoons/bugs-bunny.jpg', title: 'Bugs Bunny', overview: 'The Wabbit Who Came To Supper' },
-  { id: 1, img_src: 'http://watchingmovies.getenjoyment.net/images/cartoons/bugsy.jpg', title: 'Bugs Bunny', overview: 'Falling Hare' },
-  { id: 2, img_src: 'http://watchingmovies.getenjoyment.net/images/cartoons/daffyx.png', title: 'Looney Tunes', overview: 'Confusions Nutzy Spy' }
-]
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rows: [
-        <p>This is my row</p>
-      ]
      };
+// this.performSearch("ant man")
   }
 
-componentWillMount() {
-  var movieRows = [];
-  movies.map(movie => {
-    const movieRow = <MovieRow
-    movie={movie}
-    />
-   movieRows.push(movieRow)
-  })
-  this.setState({ rows: movieRows});
-}
+  componentWillMount() {
+this.performSearch("ant man")
+  }
+
+//   performSearch = (searchTerm) => {
+//     const urlString = "https://api.themoviedb.org/3/search/movie?api_key=1b93c0b51183698174efd96f0c4507b6&query=" + searchTerm
+//    axios.get(urlString)
+//      .then(res => {
+//        const movies = res.data;
+//        console.log(movies);
+// // console.log(res);
+// const movieRows = [];
+// movies.forEach(movie => {
+//            movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+//                    console.log(movie.poster_path)
+//                    const movieRow = <MovieRow key={movie.id} movie={movie}/>
+//                    movieRows.push(movieRow)
+// })
+//        this.setState({
+//          rows: movieRows
+//        });
+//      })
+//      .catch(err => console.log(err));
+//  }
+
+
+  performSearch(searchTerm) {
+    console.log("Perform search using moviedb")
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=1b93c0b51183698174efd96f0c4507b6&query=" + searchTerm
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully")
+        console.log(searchResults)
+        const results = searchResults.results
+        console.log(results[0])
+
+        var movieRows = []
+
+        results.map((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          console.log(movie.poster_path)
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data")
+      }
+    })
+  }
+
 
   render() {
     return (
@@ -48,8 +87,9 @@ componentWillMount() {
   </tbody>
 </table>
 <input className="input" type="text" placeholder="Search..." />
-
-{this.state.rows}
+      {
+        this.state.rows
+      }
     </div>
     );
   }
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class MovieRow extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  };
+class MovieRow extends React.Component {
+  viewMovie() {
+    // console.log("Trying to view movie")
+    // console.log(this.props.movie.title)
+    const url = "https://www.themoviedb.org/movie/" + this.props.movie.id
+    window.location.href = url
   }
+
   render() {
-    return (
-<div>
-      const movieRow = <table>
-    <tbody key={this.props.movie.id}>
-  <tr>
-    <td>
-            <img className="poster" alt="poster" src={this.props.movie.img_src} />     </td>           <td>             <h2>{this.props.movie.title}</h2>           </td>
-          <td>
-            <p>{this.props.movie.overview}</p>
-          </td>
-  </tr>
-</tbody>     </table>
-</div>
-    );
+    return <table key={this.props.movie.id}>
+    <tbody>
+      <tr>
+        <td>
+          <img alt="poster" width="120" src={this.props.movie.poster_src}/>
+        </td>
+        <td>
+          <h3>{this.props.movie.title}</h3>
+          <p>{this.props.movie.overview}</p>
+          <input type="button" onClick={this.viewMovie.bind(this)} value="View"/>
+        </td>
+      </tr>
+    </tbody>
+  </table>
   }
 }
 
-export default MovieRow;
+export default MovieRow
