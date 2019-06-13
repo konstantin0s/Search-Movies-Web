27d0ceb72bdce4b03e30d08513bb0cc075982457
konstantin0s <constantintofan85@gmail.com>
2019-06-13 12:54:07 +0200

    Add view button

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 class MovieRow extends React.Component {
-  viewMovie() {
+
+  viewMovie = () => {
     // console.log("Trying to view movie")
     // console.log(this.props.movie.title)
     const url = "https://www.themoviedb.org/movie/" + this.props.movie.id
@@ -9,16 +10,16 @@ class MovieRow extends React.Component {
   }
 
   render() {
-    return <table key={this.props.movie.id}>
-    <tbody>
-      <tr>
+    return <table className="container" key={this.props.movie.id}>
+    <tbody className="row">
+      < tr className = "col col-md-12" >
         <td>
           <img alt="poster" width="120" src={this.props.movie.poster_src}/>
         </td>
         <td>
           <h3>{this.props.movie.title}</h3>
-          <p>{this.props.movie.overview}</p>
-          <input type="button" onClick={this.viewMovie.bind(this)} value="View"/>
+          <p className="overview">{this.props.movie.overview}</p>
+          <input className="btn btn-primary" type="button" onClick={this.viewMovie} target="_blank" value="View"/>
         </td>
       </tr>
     </tbody>
