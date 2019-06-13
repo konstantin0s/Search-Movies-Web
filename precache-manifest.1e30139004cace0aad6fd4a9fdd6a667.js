c239d3b41c73cdc8c9c9ae289a37627f11340406
konstantin0s <constantintofan85@gmail.com>
2019-06-13 13:35:44 +0200

    Updates

---
+++ precache-manifest.1e30139004cace0aad6fd4a9fdd6a667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7078e153e34122d54b839f3b3350ae04",
+    "url": "/Search-Movies-Web/index.html"
+  },
+  {
+    "revision": "f7aed2535ec63611a446",
+    "url": "/Search-Movies-Web/static/css/main.f9541ba7.chunk.css"
+  },
+  {
+    "revision": "069c68944396ac2bcc36",
+    "url": "/Search-Movies-Web/static/js/2.b1b15d26.chunk.js"
+  },
+  {
+    "revision": "f7aed2535ec63611a446",
+    "url": "/Search-Movies-Web/static/js/main.17e919a2.chunk.js"
+  },
+  {
+    "revision": "039f7c4ecea885a6549c",
+    "url": "/Search-Movies-Web/static/js/runtime~main.85e4e736.js"
+  },
+  {
+    "revision": "67abc369f67ac0403165c69c375a9cc3",
+    "url": "/Search-Movies-Web/static/media/cartoon.67abc369.png"
+  }
+]);
